42f83396de887ab199bcc5c49a6e76e9a68459dd
Tecnico - TI <tecnico@epfserver.com.br>
2017-03-27 21:41:29 -0300

    Criação do projeto com as classes principais

---
+++ src/TV.java
@@ -0,0 +1,71 @@
+
+public class TV {
+	private int canal;
+	private String modelo;
+	private boolean ligada;
+	private int volume;
+	
+	public TV(int canal, String modelo, boolean ligada, int volume) {
+		this.canal = canal;
+		this.modelo = modelo;
+		this.ligada = ligada;
+		this.volume = volume;
+	}
+	
+	public void aumentarVolume() {
+		this.volume = this.volume + 1;
+	}
+	
+	public void diminuirVolume() {
+		this.volume = this.volume - 1;
+	}
+	
+	public void ligar(){
+		this.ligada=true;
+	}
+	
+	public void desligar(){
+		this.ligada=false;
+	}
+	
+	public void aumentarCanal(){
+		this.canal = this.canal + 1;
+	}
+	
+	public void diminuirCanal(){
+		this.canal = this.canal - 1;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public boolean isLigada() {
+		return ligada;
+	}
+
+	public void setLigada(boolean ligada) {
+		this.ligada = ligada;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
+	public int getCanal(){
+		return canal; 
+	}
+	
+	public void setCanal(int canal) {
+		this.canal = canal;
+	}
+	
+	public void setModelo(String modelo){
+		this.modelo = modelo;
+	}
+	
+}
---
+++ src/funcionaTV.java
@@ -0,0 +1,31 @@
+//import java.util.Scanner;
+
+public class funcionaTV {
+
+	public static void main(String[] args) {
+			
+		TV TVdaSala = new TV(135, "LG", true, 20);
+		
+		System.out.println("Canal: " + TVdaSala.getCanal());
+		System.out.println("Modelo: " + TVdaSala.getModelo());
+		System.out.println("Ligada: " + TVdaSala.isLigada());
+		System.out.println("Volume: " + TVdaSala.getVolume());
+
+		TV TVdoQuarto = new TV(500, "Samsung", false, 0);
+		
+		
+		System.out.println("\n");
+		System.out.println("Canal: " + TVdoQuarto.getCanal());
+		System.out.println("Modelo: " + TVdoQuarto.getModelo());
+		TVdoQuarto.setModelo("Philco");
+		System.out.println("Modelo foi trocado para: " + TVdoQuarto.getModelo());
+		System.out.println("Ligada: " + TVdoQuarto.isLigada());
+		System.out.println("Volume: " + TVdoQuarto.getVolume());
+
+		
+		
+		
+	}
+	
+
+}
